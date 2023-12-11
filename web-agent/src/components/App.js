53840f0bad8e070b48b68aaf6e352efc25585690
Huy Pham <nylespham@Huys-MacBook-Air.local>
2023-12-11 21:50:56 +0700

    Web-agent initiated

---
+++ web-agent/src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navigation from "./Navigation";
+import Footer from "./Footer";
+import Home from "./Home";
+
+function App() {
+  return (
+    <div>
+    <Navigation />
+    <Home />
+    <Footer />
+    </div>
+  );
+}
+
+export default App;
