14300c2fcf91c3d1b9b1a22c939cb562963477b1
Huy Pham <nylespham@Huys-MacBook-Air.local>
2023-12-18 16:06:25 +0700

    request validator drafted

--- login-oauth/src/routes/signin.ts
+++ login-oauth/src/routes/signin.ts
@@ -1,20 +1,18 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { validateRequest } from "../middlewares/validate-request";
 
 const router = express.Router();
 
 router.post("/users/signin", [
     body("username").isEmail().withMessage("Username must be an email"),
     body("password").trim().notEmpty().withMessage("Password cannot be empty")
-], (req: Request, res: Response) => {
+], 
+validateRequest,
+(req: Request, res: Response) => {
     const result = validationResult(req);
     const { username, password } = req.body
-    if (result.isEmpty()){
-        console.log(username, password);
-        res.send("Login successfully!");
-    }
-
-    res.send({errors: result.array()})
+    console.log(username, password);
 })
 
 export { router as signinRouter };
